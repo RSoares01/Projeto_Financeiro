9e7be0d6a48f1263df13c177f395804ce4a2da73
Giovanna Martins <RafaelSoares16032001@gmail.com>
2025-09-19 17:08:08 -0300

    Commit inicial, subindo camadas para arquitetura e entidades das tabelas Transacoes e Categorias

---
+++ Projeto_Financeiro.Domain/Entity/Categorias.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Financeiro.Domain.Entity
+{
+    public class Categorias
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public char Tipo { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
---
+++ Projeto_Financeiro.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Projeto_Financeiro.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
